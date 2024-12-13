a7d9b2519c24d5ca960d8296da9ce2063e2984ed
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2024-12-13 21:39:32 +0545

    handling wrong user input

--- TOC-Practical-Questions/4. PDA-accepting-0n1n/pda-0n1n.js
+++ TOC-Practical-Questions/4. PDA-accepting-0n1n/pda-0n1n.js
@@ -13,30 +13,34 @@ for (let i=0; i< input.length; i++)
 {
     //  console.log('inside for loop');
     //   console.log('input', input[0]);
+   if (input[i] === '0' || input[i] === '1')  //to make sure input is only boolean
+   {
     if (state === 'q0' && input[i] === '0' && top == -1 || input[i] === '0' && stack[top]==='0')
-       {
-           top++;
-        stack.push('0');
-         console.log('stack', stack);
-        //  console.log(stack.length);
-       }
-    else if (state === 'q0' && input[i]=== '1' && stack[top] == '0')
-       {
-        stack.pop('0'); 
-        top--;
-        state = 'q1';
-        //  console.log('haha', stack.length); --> this is called debugging using console dude 😩, took me 5 minutes to find the error 🤦‍♀️🤣
-       }
-    else if (state === 'q1' && input[i]=== '1' && stack[top] == '0')
-      {  stack.pop('0');
-        //  console.log('hehe',stack.length);
+      {
+          top++;
+       stack.push('0');
+        console.log('stack', stack);
+       //  console.log(stack.length);
       }
-    else{
-        state = 'reject';
-        //  console.log('reject state');
-        break;
-
-    }
+   else if (state === 'q0' && input[i]=== '1' && stack[top] == '0')
+      {
+       stack.pop('0'); 
+       top--;
+       state = 'q1';
+       //  console.log('haha', stack.length); --> this is called debugging using console dude 😩, took me 5 minutes to find the error 🤦‍♀️🤣
+      }
+   else if (state === 'q1' && input[i]=== '1' && stack[top] == '0')
+     {  stack.pop('0');
+       //  console.log('hehe',stack.length);
+     }
+   else{
+       state = 'reject';
+       //  console.log('reject state');
+       break;
+   }
+   }
+   else
+   console.log("Wrong input");
 }
 
  console.log(stack.length);
