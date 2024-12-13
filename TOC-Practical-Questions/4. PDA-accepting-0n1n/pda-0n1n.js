e28acb721b545b5c865dc013a7ad1845213d3497
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2024-12-13 17:29:20 +0545

    PDA accepting 0^n 1^n implemented successfully ✅

---
+++ TOC-Practical-Questions/4. PDA-accepting-0n1n/pda-0n1n.js
@@ -0,0 +1,47 @@
+let stack = [];   //stack for our PDA
+
+//----> taking input from user
+
+let input = prompt("Enter the string: ");
+ console.log(input);
+  console.log(input.length);
+ console.log(stack.length);
+let top = -1;    //initializing top 
+let state = 'q0';
+
+for (let i=0; i< input.length; i++)
+{
+    //  console.log('inside for loop');
+    //   console.log('input', input[0]);
+    if (state === 'q0' && input[i] === '0' && top == -1 || input[i] === '0' && stack[top]==='0')
+       {
+           top++;
+        stack.push('0');
+         console.log('stack', stack);
+        //  console.log(stack.length);
+       }
+    else if (state === 'q0' && input[i]=== '1' && stack[top] == '0')
+       {
+        stack.pop('0'); 
+        top--;
+        state = 'q1';
+        //  console.log('haha', stack.length); --> this is called debugging using console dude 😩, took me 5 minutes to find the error 🤦‍♀️🤣
+       }
+    else if (state === 'q1' && input[i]=== '1' && stack[top] == '0')
+      {  stack.pop('0');
+        //  console.log('hehe',stack.length);
+      }
+    else{
+        state = 'reject';
+        //  console.log('reject state');
+        break;
+
+    }
+}
+
+ console.log(stack.length);
+if (state !== 'reject' && stack.length === 0)
+console.log('string is accepted!');
+else
+console.log('string is not accepted.');
+
---
+++ TOC-Practical-Questions/4. PDA-accepting-0n1n/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+for (let i=0; i< input.length; i++)
+// {
+//     if (state === 'q0' && input[i] === '0' && stack[top] == '' || input[i] === '0' && stack[top]==='0')
+//        {
+//            top++;
+//         stack.push('0');
+//        }
+//     else if (state === 'q0' && input[i]=== '1' && stack[top] == '0')
+//        {
+//         stack.pop('0'); 
+//         state = q1;
+//        }
+//     else if (state === 'q1' && input[i]=== '1' && stack[top] == '0')
+//         stack.pop('0');
+// }
